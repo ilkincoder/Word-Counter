e9fdd52721d49e572c62b7f3bb3433b8c889c1b7
Ilkin Hamzayev <ilkinhamzayev925@gmail.com>
2024-01-30 21:29:16 +0100

    Done

---
+++ script.js
@@ -0,0 +1,62 @@
+// Getting the dom elements
+
+text = document.getElementById('area');
+word = document.getElementById('word');
+char = document.getElementById('char');
+
+
+// On input implementing function
+
+
+text.addEventListener('input', function(){
+
+// Updating character count
+
+let characters = this.value;
+char.textContent = characters.length;
+
+//Removing spaces from beginning and end
+
+characters.trim();
+
+//  create andreturn array of singular words using (/\s/) - regex pattern
+
+let wordlist = characters.split(/\s/);
+
+let words = wordlist.filter(function(element){
+    return element != "";
+})
+
+
+word.textContent = words.length;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
